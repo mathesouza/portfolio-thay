88ee05ce4415a7ab3a6199e2c60d192a505c4b31
Matheus Sabino <matheusouzatj@gmail.com>
2019-05-20 09:47:34 -0300

    removed base.js

--- src/base.example.js
+++ src/base.example.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey: "AIzaSyB9BSN58F1KojMqVcCMHocpDVdU47guF20",
-  authDomain: "portfolio-thay.firebaseapp.com",
-  databaseURL: "https://portfolio-thay.firebaseio.com",
-  projectId: "portfolio-thay",
-  storageBucket: "portfolio-thay.appspot.com",
-  messagingSenderId: "501851486038"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 const Rebase = require("re-base");
