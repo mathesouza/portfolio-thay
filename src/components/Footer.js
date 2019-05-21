54986d22230c2d66452cb89c2121131e987bbc99
Matheus Sabino <matheusouzatj@gmail.com>
2019-05-21 09:15:05 -0300

    created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="footer">
-    <div className="container container-footer">
-      <a href='https://www.instagram.com/pand.arte/?hl=pt-br'>
-        <i class="fab fa-instagram fa-5x" ></i>
-          <span>
-            @pand.art
-          </span>
-      </a>
-      
+<footer class="page-footer font-small footer">
+
+  <div class="container container-footer ">
+
+    <div class="row">
+
+      <div class="col-md-12">
+        <img src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/616c5e67978855.5b4d3661ea12f.png' height='200' ></img>
+        <div class="mb-5 flex-center">
+          <a class="fb-ic" href='https://www.facebook.com/PandaThay'>
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a class="li-ic" href='https://www.linkedin.com/in/thayn%C3%A1-melo/'>
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a class="ins-ic" href='https://www.instagram.com/pand.arte/' >
+            <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <a class="pin-ic" href='https://br.pinterest.com/minissauro/'>
+            <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+          </a>
+        </div>
+      </div>
+
     </div>
-  </footer>
+
+  </div>
+
+</footer>
 );
 
 export default Footer;
